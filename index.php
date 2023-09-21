b4f4b21c152040039e5619d3c111bd7f135a3569
Arthur Vitor <109088305+dinnaky@users.noreply.github.com>
2023-09-21 15:07:23 -0300

    PHP COMMIT

---
+++ index.php
@@ -0,0 +1,83 @@
+<?php
+
+/*aqui vamos conectar 
+com o banco 
+de dados*/
+$servername = "localhost";
+//você deu nome ao banco de dados
+$database = "func2c";
+$username = "root";
+$password = "";
+
+$conexao = mysqli_connect(
+    $servername, $username, 
+    $password,$database
+);
+
+if (!$conexao){
+    die("Falha na conexão".mysqli_connect_error());
+}
+// echo "E essa palheta ai bb ;)  :P";
+// echo  $id." ".$nome." ".$cpf." ".$botao;
+
+$id = $_POST["id"];
+$nome = $_POST["nome"];
+$cpf = $_POST["cpf"];
+$botao = $_POST["botao"];
+
+
+$sql = "INSERT INTO funcionarios (id, nome, cpf) 
+        VALUES ('', '$nome', '$cpf')";
+
+if($botao == "Cadastrar"){
+    if (mysqli_query($conexao, $sql)){
+        echo "Cadastrado com sucesso!";
+    }else{
+        echo "Falha ao cadastrar!";
+    }
+}
+
+
+
+?>
+
+<html>
+    <head>
+        <style>
+
+            form {
+                display: flex;
+                flex-direction: column;
+            }
+
+            input {
+                width: 400px;
+                margin: 10px 0;
+            }
+
+            .buttons {
+                display: flex;
+                justify-content: space-around;
+                width: 400px;
+            }
+
+            .buttons input {
+                width: 80px;
+            }
+        </style>
+    </head>
+    <body>
+        <form name="func" method = "POST">
+            <label for="id">ID:</label> 
+            <input type="text" name = "id">
+            <label for="nome">NOME:</label> 
+            <input type="text" name = "nome">
+            <label for="cpf">CPF:</label> 
+            <input type="text" name = "cpf">
+                <div class="buttons">
+                    <input type="submit" name = "botao" value="Cadastrar">
+                    <input type="reset" name = "botao" value="Cancelar">
+                </div>
+        </form>
+    </body>
+</html>
